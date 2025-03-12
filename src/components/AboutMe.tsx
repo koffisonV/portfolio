3778568bb6d5da7d7927e7a93813d1362432660b
Koffison Voumadi <koffison29@gmail.com>
2025-03-12 18:04:08 -0400

    Update AboutMe.tsx

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -98,7 +98,7 @@ export default function AboutMe() {
           >
             <h3 className="text-lg sm:text-xl font-semibold mb-3">Education</h3>
             <p className="text-sm sm:text-base text-foreground/70">
-              Bachelor&apos;s of Science in Computer Science from John Jay College. Professional certificates in: <br/> Agile Framework&mdash;LinkedIn Learning.<br/>IT Support Specialist&mdash;IBM.
+              B.S. in Computer Science from John Jay College. Knowledgeable in: <br/> Agile &mdash;LinkedIn Learning.<br/>IT Support Specialist&mdash;IBM.
             </p>
           </motion.div>
 
